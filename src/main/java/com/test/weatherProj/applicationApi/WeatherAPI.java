a41997268ce0a8deca726d59b79f36474ea5131f
TKShetty <taranathshetty91@gmail.com>
2021-07-26 19:50:10 +0530

    1)Added API related base structure and functionality 2) Updated config and data properties

---
+++ src/main/java/com/test/weatherProj/applicationApi/WeatherAPI.java
@@ -0,0 +1,22 @@
+package com.test.weatherProj.applicationApi;
+
+import static com.test.weatherProj.api.Route.WEATHER;
+
+import java.util.HashMap;
+
+import com.test.weatherProj.api.RestResource;
+import com.test.weatherProj.utils.DataLoader;
+
+import io.restassured.response.Response;
+
+public class WeatherAPI {
+
+	public static Response getCityWeatherAPIResponse(String cityName) {
+		HashMap<String, String> queryParamsMap = new HashMap<String, String>();
+		DataLoader dataLoader = DataLoader.getInstance();
+		queryParamsMap.put("q", cityName);
+		queryParamsMap.put("appid", dataLoader.getPropertyValue("AppId"));
+		queryParamsMap.put("units", dataLoader.getPropertyValue("Units"));
+		return RestResource.get(WEATHER, queryParamsMap);
+	}
+}
--- src/test/java/com/test/weatherProj/tests/LandingPageTest.java
+++ src/test/java/com/test/weatherProj/tests/LandingPageTest.java
@@ -1,18 +1,20 @@
 package com.test.weatherProj.tests;
 
+import static org.testng.Assert.assertEquals;
+
 import java.io.IOException;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.test.weatherProj.api.StatusCode;
+import com.test.weatherProj.applicationApi.WeatherAPI;
 import com.test.weatherProj.pages.HomePage;
-import com.test.weatherProj.pages.WeatherForecastPage;
+import com.test.weatherProj.utils.DataLoader;
+
+import io.restassured.response.Response;
 
 public class LandingPageTest extends BaseTest {
 
@@ -22,17 +24,23 @@ public class LandingPageTest extends BaseTest {
 	public void initialize() throws IOException {
 		driver = getDriver();
 	}
-	@Parameters({ "cityName" })
+
 	@Test
-	public void verifyTemperature(String cityName) {
+	public void verifyTemperature() {
+		String cityName=DataLoader.getInstance().getPropertyValue("CityName");
 		System.out.println(driver.getCurrentUrl());
 		System.out.println(driver.getTitle());
 		HomePage homePage = new HomePage(driver);
 		try {
-			WeatherForecastPage wp = homePage.searchCity(cityName);
-			String uitemp = wp.getCurrentTemperature();
+			/*WeatherForecastPage wp = homePage.searchCity(cityName);
+			String uiTempReading = wp.getCurrentTemperature();*/
+			Response response =WeatherAPI.getCityWeatherAPIResponse(cityName);
+			assertStatusCode(response.statusCode(), StatusCode.CODE_200);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+    private void assertStatusCode(int actualStatusCode, StatusCode statusCode){
+        assertEquals(actualStatusCode, statusCode.code);
+    }
 }
